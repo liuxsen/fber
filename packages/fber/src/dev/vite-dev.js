830aaeec03b9c7f4b532df4a13ef78b0d1a7e706
jianhui <liujh1@fosun.com>
2024-03-05 18:19:38 +0800

    :bug: fix(custom): require 修改

--- packages/fber/src/dev/vite-dev.js
+++ packages/fber/src/dev/vite-dev.js
@@ -1,9 +1,4 @@
 const { createServer } = require('vite')
-const react = require('@vitejs/plugin-react')
-const vue3 = require('@vitejs/plugin-vue')
-const vue3Jsx = require('@vitejs/plugin-vue-jsx')
-const vue2 = require('@vitejs/plugin-vue2')
-const vue2jsx = require('@vitejs/plugin-vue2-jsx')
 
 function getPlugins(rootdir) {
   const { checkPkgEnv } = require('../utils/checkPkgEnv')
@@ -12,16 +7,16 @@ function getPlugins(rootdir) {
   const env = checkPkgEnv(rootdir)
   const plugins = []
   if (env.isReact) {
-    plugins.push(react())
+    plugins.push(require('@vitejs/plugin-react')())
   }
 
   if (env.isVue) {
     const isVue3 = semver.gt(env.version, '3.0.0')
     if (isVue3) {
-      plugins.push([vue3(), vue3Jsx()])
+      plugins.push([require('@vitejs/plugin-vue')(), require('@vitejs/plugin-vue-jsx')()])
     }
     else {
-      plugins.push([vue2(), vue2jsx()])
+      plugins.push([require('@vitejs/plugin-vue2')(), require('@vitejs/plugin-vue2-jsx')()])
     }
   }
 
